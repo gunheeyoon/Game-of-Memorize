3768b2829a57bd43e8b6a566400aea44ef3c4272
gunheeyoon <gunheeyoon@outlook.com>
2023-09-03 10:23:47 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by GUNHEE YOON on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
